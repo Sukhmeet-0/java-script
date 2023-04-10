e49d69bd0b4f55d2f8df34edd28a5902b0e52393
Sukhmeet Singh <sukhmeets111@gmail.com>
2023-04-10 19:09:38 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,22 @@
+// nn bb ss u  primitive data type
+let a=null;
+let b=78;
+let c=true;
+let d=BigInt("939");
+let e="hi";
+let f=Symbol("I am sukhmeet");
+let g=undefined;
+console.log(a,b,c,d,e,f,g);
+console.log(typeof(d));
+console.log(typeof(a));
+console.log(typeof(g));
+
+
+//object
+const item={
+    "Harry":true,
+    "sukh":false,
+    "Love":93,
+    "rohan":undefined
+}
+console.log(item["sukh"])
---
+++ variables.js
@@ -0,0 +1,17 @@
+var i="Hello";
+console.log(i);
+const ii="Bye";
+console.log(ii);
+
+let a=67;
+console.log(a);
+b=38;
+console.log(b);
+{
+    let a=90;
+    console.log(a);
+    var i="Yo";
+    console.log(i);
+}
+console.log(a);
+console.log(i);
