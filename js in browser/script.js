ef59531e680dcaa961a0bfa7b3a439ae27be61f1
Sukhmeet-0 <sukhmeets111@gmail.com>
2023-04-29 19:44:22 +0530

    adding js on browser console function

---
+++ js in browser/script.js
@@ -0,0 +1,27 @@
+// console.log(console)
+
+// console.error("Error")
+
+console.assert(55>53)
+console.clear()
+
+abj={a:1,b:2,c:3}
+console.table(abj)
+
+console.warn("Hey")
+
+console.info("important info")
+console.log("console")
+console.warn("warn")
+console.assert(55<7)
+console.error("err")
+
+console.time()
+console.time("a")
+console.timeEnd("a")
+
+console.time("forloop")
+for(let i=0;i<5;i++){
+    console.log(223)
+}
+console.timeEnd("forloop")
