69b89ad074866255d2156329e832c31e954f5c16
Sukhmeet-0 <sukhmeets111@gmail.com>
2023-05-01 20:58:21 +0530

    adding js alert prompt

--- js in browser/script.js
+++ js in browser/script.js
@@ -1,27 +1,51 @@
-// console.log(console)
+// // console.log(console)
 
-// console.error("Error")
+// // console.error("Error")
 
-console.assert(55>53)
-console.clear()
+// console.assert(55>53)
+// console.clear()
 
-abj={a:1,b:2,c:3}
-console.table(abj)
+// abj={a:1,b:2,c:3}
+// console.table(abj)
 
-console.warn("Hey")
+// console.warn("Hey")
 
-console.info("important info")
-console.log("console")
-console.warn("warn")
-console.assert(55<7)
-console.error("err")
+// console.info("important info")
+// console.log("console")
+// console.warn("warn")
+// console.assert(55<7)
+// console.error("err")
 
-console.time()
-console.time("a")
-console.timeEnd("a")
+// console.time()
+// console.time("a")
+// console.timeEnd("a")
 
-console.time("forloop")
-for(let i=0;i<5;i++){
-    console.log(223)
-}
-console.timeEnd("forloop")
+// console.time("forloop")
+// for(let i=0;i<5;i++){
+//     console.log(223)
+// }
+// console.timeEnd("forloop")
+// alert("hi i am alerting you")
+
+
+
+
+// alert("Enter the value of a!")
+// let a = prompt("Enter a here","578")
+// a=Number.parseInt(a)
+// // document.write(a)
+// alert("You entered a of type "+ typeof a)
+// let write = confirm("Do you want to write it to the page")
+// if(write){
+//     document.write(a)
+// }else{
+//    document.write("Please allow me to write") 
+// }
+// document.write(a)
+
+
+// console.log(window)
+// window.console.log()
+// window.alert()
+
+document.body.style.background="yellow"
